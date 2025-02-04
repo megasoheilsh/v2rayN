53d466c97f0bb1dbb188918f899bcb5cebc14231
megasoheilsh <soheilshokrollahinia@gmail.com>
2025-02-04 19:33:50 +0330

    Update CoreInfoHandler.cs

--- v2rayN/ServiceLib/Handler/CoreInfoHandler.cs
+++ v2rayN/ServiceLib/Handler/CoreInfoHandler.cs
@@ -154,7 +154,7 @@ namespace ServiceLib.Handler
                 {
                     CoreType = ECoreType.sing_box,
                     CoreExes = ["sing-box-client", "sing-box"],
-                    Arguments = "run -c config.json --disable-color",
+                    Arguments = "run -c sing_box.json --disable-color",
                     Url = GetCoreUrl(ECoreType.sing_box),
 
                     ReleaseApiUrl = urlSingbox.Replace(Global.GithubUrl, Global.GithubApiUrl),
